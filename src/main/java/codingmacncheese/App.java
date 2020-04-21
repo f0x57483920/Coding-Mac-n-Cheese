8838e24dc43eab4248a82d9b403395e13a18d805
f0x57483920 <senordaschweichert@googlemail.com>
2020-04-21 23:57:43 +0200

    Test

--- src/main/java/codingmacncheese/App.java
+++ src/main/java/codingmacncheese/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java  " + javaVersion + "And Coding & Mac' n Cheese is ready to go!.");
 
         var scene = new Scene(new StackPane(label, new Label("EZ PZ")), 640, 480);
         stage.setScene(scene);
